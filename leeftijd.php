db381b14dbae6ee5375acca55961763b3b4aef6d
sohrabramzi <sohrab.ramzi1@gmail.com>
2014-09-09 14:15:57 +0200

    leeftijd

---
+++ leeftijd.php
@@ -0,0 +1,23 @@
+<html>
+	<form action="" method="post">
+	Leeftijd: <input type="text" name="Leeftijd"><br>
+	<input type="submit">
+	</form>
+</html>
+
+<?php
+
+	$leeftijd = $_POST['Leeftijd'];
+
+	if ($leeftijd >= 16 && $leeftijd < 18) {
+		  echo "Je mag scooter rijden";
+
+	} elseif ($leeftijd >= 18 && $leeftijd < 21) {
+  	echo "Je mag auto rijden";
+
+	} elseif ($leeftijd>=21){
+  	echo "Je mag alles doen";
+	}
+
+
+?>
